85ae5f476de3bd604310d447ce7a5f1646f26517
Jeremy Mikkola <jeremy@jeremymikkola.com>
2025-05-11 08:57:04 -0400

    Get the 'digits' parser working

--- app/Main.hs
+++ app/Main.hs
@@ -64,6 +64,7 @@ data Type
 --
 
 
+-- TODO: This is the wrong monad, the text needs to be passed as state
 type Parser a = String -> Either String (a, String)
 
 
@@ -73,28 +74,38 @@ char c s = case s of
   _                 -> Left $ "Cannot match char " ++ [c]
 
 
-string :: String -> Parser String
-string x s =
-  let len = length x
-      matching = take len s
-      rest = drop len s
-  in if matching == x
-     then Right (x, rest)
-     else Left $ "Cannot match string " ++ x
+oneOf :: [Char] -> Parser Char
+oneOf options s = matchOneOf options
+  where err = "Cannot match any of " ++ options
+        matchOneOf []     = Left err
+        matchOneOf (c:cs) = case s of
+           (c':rest) | c' == c -> Right (c, rest)
+           _                   -> matchOneOf cs
 
 
-oneOf :: [Parser a] -> Parser a
-oneOf []     _ = Left "oneOf requires at least one parser"
-oneOf [p]    s = p s
-oneOf (p:ps) s =  p s <> oneOf ps s
+optional :: Parser a -> Parser (Maybe a)
+optional p s = case p s of
+  Left  _       -> Right (Nothing, s)
+  Right (r, s') -> Right (Just r,  s')
 
+many :: Parser a -> Parser [a]
+many p s = Right $ parseMany s
+  where parseMany str = case p str of
+            Left  _         -> ([], str)
+            Right (r, str') ->
+               let (rs, rest) = parseMany str'
+               in (r : rs, rest)
 
+many1 :: Parser a -> Parser [a]
+many1 p s = do
+  (a1, s') <- p s
+  (as, s'') <- many p s'
+  return $ (a1 : as, s'')
 
+digit :: Parser Char
+digit = oneOf ['0' .. '9']
 
-
-
-
-
-
+digits :: Parser String
+digits = many1 digit
 
 

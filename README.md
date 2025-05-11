b28d7edef951a3863b89d3b331bcd25b15e98c52
Jeremy Mikkola <jeremy@jeremymikkola.com>
2025-05-10 22:07:38 -0400

    Start on a parser

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# goic
+
+The name comes from merging "Go" and "c" (plus another letter to make it sound better).
+The idea is to build a small c-like language with more go-like syntax.
+
+Current status: No code written.
+
+The eventual goal is to try to learn x86_64 assembly.
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,100 @@
 module Main where
 
+-- import Control.Applicative ((<|>))
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+--
+-- Define the types for the language's AST
+--
+
+data Program = Program [Module]
+    deriving (Show)
+
+data Module = Module String [Function]
+    deriving (Show)
+
+data Function = Function String FnType Statement
+    deriving (Show)
+
+data Statement
+    = EStatement Expression
+    | Return Expression
+    | BareReturn
+    | NewVar String Expression
+    | If Expression Statement Statement
+    | While Expression Statement
+    | Block [Statement]
+    deriving (Show)
+
+data Expression
+    = Literal Value
+    | BinaryOp Op Expression Expression
+    | Call Expression [Expression]
+    deriving (Show)
+
+data Value
+    = VInt Int
+    | VString String
+    deriving (Show)
+
+data Op
+    = Plus
+    | Minus
+    | Times
+    | Divide
+    | And
+    | Or
+    deriving (Show)
+
+data FnType = FnType [Type] Type
+    deriving (Show)
+
+data Type
+    = Func FnType
+    | Void
+    | Int
+    | String
+    | Char
+    deriving (Show)
+
+--
+-- Parser for the basic language
+--
+
+
+type Parser a = String -> Either String (a, String)
+
+
+char :: Char -> Parser Char
+char c s = case s of
+  (c2:s') | c2 == c -> Right (c, s')
+  _                 -> Left $ "Cannot match char " ++ [c]
+
+
+string :: String -> Parser String
+string x s =
+  let len = length x
+      matching = take len s
+      rest = drop len s
+  in if matching == x
+     then Right (x, rest)
+     else Left $ "Cannot match string " ++ x
+
+
+oneOf :: [Parser a] -> Parser a
+oneOf []     _ = Left "oneOf requires at least one parser"
+oneOf [p]    s = p s
+oneOf (p:ps) s =  p s <> oneOf ps s
+
+
+
+
+
+
+
+
+
+
+
